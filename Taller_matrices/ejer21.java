e87c53d6c51bfd6d0a21f59d3e8209cd93289f34
Hanniii <jhoannis205@gmail.com>
2026-09-13 16:35:15 -0500

    taller

--- Taller_matrices/ejer21.java
+++ Taller_matrices/ejer21.java
@@ -3,9 +3,37 @@ public class ejer21 {
         //Pregunta 21: Dada una matriz de M*M elementos, hacer un programa que construya un vector B,
         //donde cada uno de sus componentes sea la suma de los elementos de valores
         //numéricos pares de las filas de la matriz.
-        
-        
-        
 
+        int [][] matriz =  
+        {{2, 4, 6},
+         {3, 6, 7},       
+         {8, 9, 0}};
+
+        int M = matriz.length;                    
+        int[] B = new int[M];
+        
+        for (int i = 0; i < M; i++) {
+            int sumaPares = 0;   
+            for (int j = 0; j < M; j++) {
+                if (matriz [i][j] % 2 == 0){
+                    sumaPares += matriz [i][j];
+                }
+        B[i]= sumaPares;        
+        }
+    }
+    
+        System.out.println("Matriz A: ");
+        System.out.println();
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(matriz[i][j]+ "|");
+      }
+      System.out.println();
+    }
+    System.out.println("--------------->");
+    System.out.println("Matriz B (suma de pares): ");
+        for (int i = 0; i < M; i++) {
+            System.out.println("B[" + i + "] = " + B[i]);
+        }
     }
 }
---
+++ Taller_matrices/ejer22.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class ejer22 {
+    public static void main(String[] args) {
+        //Pregunta 22:  Escribir un programa que lea las dimensiones de una matriz, lea y visualice la matriz y a
+        //continuación encuentre el mayor y menor elemento de la matriz y sus posiciones.
+    
+        Scanner tc = new Scanner(System.in);
+
+        System.out.print("Ingrese el numero de filas: ");
+        int filas = tc.nextInt();
+        System.err.print("Ingrese el numero de columnas: ");
+        int columnas = tc.nextInt();
+
+        int [][] matriz = new int [filas][columnas];
+
+        System.out.println();
+        System.out.println("Ingrese los elementos de la matriz dicha: ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("Matriz [" + i + "][" + j + "]: ");
+                matriz [i][j] = tc.nextInt();
+            }
+        }
+        System.out.println();
+        System.out.println("Matriz Ingresada: ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+        int mayor = matriz [0][0];
+        int menor = matriz [0][0];
+        int filaMa = 0;
+        int filaMe = 0;
+        int columnaMa = 0;
+        int columnaMe = 0;
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (matriz[i][j] > mayor)  {
+                    mayor = matriz[i][j];
+                    filaMa = i;
+                    columnaMa = j;
+                }
+                if (matriz[i][j] < menor){
+                if (matriz[i][j] > mayor)  {
+                    menor = matriz[i][j];
+                    filaMe = i;
+                    columnaMe = j;
+                }         
+                }
+            }
+        }
+        System.out.println();
+        System.out.print("El mayor elemento de la matriz es: " + mayor + ", en la posicion: [" + filaMa + "]["+ columnaMa + "]");
+        System.out.println();
+        System.out.print("El mayor elemento de la matriz es: " + menor + ", en la posicion: [" + filaMe + "]["+ columnaMe + "]");
+
+        tc.close();
+    }
+}
